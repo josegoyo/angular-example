3e0c6543b19dcaebd4e8c56bcfa76405fce5f3c0
Jose Gregorio <jgregorio2791@gmail.com>
2020-06-10 01:52:26 -0600

    Components Part 2

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 import * as data from '../resource/data.json'; // my data.json file contain all data to show on the view.
 
+const DISHES: Dish[] = data.dishes;
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -9,7 +11,8 @@ import * as data from '../resource/data.json'; // my data.json file contain all 
 })
 export class MenuComponent implements OnInit {
 
-  dishes: Dish[] = data.dishes;
+  dishes: Dish[] = DISHES;
+  selectedDish: Dish = DISHES[0];
 
   constructor() { }
 
